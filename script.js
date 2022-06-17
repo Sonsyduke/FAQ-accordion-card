a6be6dfada44881541845feeddc8b98a0cfc76cc
Douglas <douglaslombard29@gmail.com>
2022-06-17 13:44:38 +0200

    The JS working for the first question

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const question = document.querySelector(".ques");
+const answer = document.querySelector(".answer");
+const arrow = document.querySelector(".arrow");
 
+question.addEventListener("click", function () {
+  answer.classList.toggle("hidden");
+  question.classList.toggle("activeQuestion");
+  arrow.classList.toggle("arrowActive");
+});
