dbdd35f0066aac2aaa57d3f16f6c17904018175e
Douglas <douglaslombard29@gmail.com>
2022-06-19 15:04:19 +0200

    Final project

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const question = document.querySelector(".ques");
-const answer = document.querySelector(".answer");
-const arrow = document.querySelector(".arrow");
+const accordion_btns = document.querySelectorAll(".accordion-header");
+const accordion_contents = document.querySelectorAll(".accordion-body");
 
-question.addEventListener("click", function () {
-  answer.classList.toggle("hidden");
-  question.classList.toggle("activeQuestion");
-  arrow.classList.toggle("arrowActive");
+accordion_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    accordion_contents.forEach((acc) => {
+      if (
+        e.target.nextElementSibling !== acc &&
+        acc.classList.contains("active")
+      ) {
+        acc.classList.remove("active");
+        accordion_btns.forEach((btn) => btn.classList.remove("active"));
+      }
+    });
+    const panel = btn.nextElementSibling;
+    panel.classList.toggle("active");
+    btn.classList.toggle("active");
+  });
 });
+
+// window.onclick = (e) => {
+//   if (!e.target.matches(".accordion-header")) {
+//     accordion_btns.forEach((btn) => btn.classList.remove("active"));
+//     accordion_contents.forEach((acc) => acc.classList.remove("active"));
+//   }
+// };
